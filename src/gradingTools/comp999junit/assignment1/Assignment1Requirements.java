0b7b2728053a7dcfaba934fbb1d3a37ceabc03c9
dewan <dewan@cs.unc.edu>
2024-04-18 19:59:15 -0400

    999Requirments

--- src/gradingTools/comp999junit/assignment1/Assignment1Requirements.java
+++ src/gradingTools/comp999junit/assignment1/Assignment1Requirements.java
@@ -1,7 +1,7 @@
 package gradingTools.comp999junit.assignment1;
 
 import grader.junit.AJUnitProjectRequirements;
-import gradingTools.comp999junit.assignment1.testcases.CartesianPointSuite;
+import gradingTools.comp999.assignment1.testcases.CartesianPointSuite;
 
 
 public class Assignment1Requirements extends AJUnitProjectRequirements{
